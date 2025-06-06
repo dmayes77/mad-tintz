0266648af518af8c9b4aa819343688e40a69571f
David Mayes <dmayes77@gmail.com>
2025-06-06 00:23:08 -0400

    working on home page contact section

--- components/ui/ContactCard.js
+++ components/ui/ContactCard.js
@@ -1,10 +1,17 @@
+// components/ContactCard.jsx
+import React from "react";
 import { businessInfo as business } from "@/content/businessInfo";
 import NextLink from "next/link";
 
-export default function ContactCard() {
-  return (
-    <div className="grid grid-cols-1 gap-6 laptop:grid-cols-3">
-      <div className="flex flex-col items-center gap-4 p-6 text-center">
+const content = {
+  sections: [
+    {
+      title: "Email",
+      description:
+        "For inquiries, reach out to us anytime at your convenience.",
+      linkText: business.email,
+      linkHref: `mailto:${business.email}`,
+      icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -19,14 +26,14 @@ export default function ContactCard() {
             d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
           />
         </svg>
-
-        <h3 className="text-accent font-semibold tracking-wide">Email</h3>
-        <p>For inquiries, reach out to us anytime at your convenience.</p>
-        <NextLink href={`mailto:${business.email}`} className="underline">
-          {business.email}
-        </NextLink>
-      </div>
-      <div className="flex flex-col items-center gap-4 p-6 text-center">
+      ),
+    },
+    {
+      title: "Phone",
+      description: "Call us today for a free consultation and quote!",
+      linkText: `+1 ${business.phone}`,
+      linkHref: `tel:${business.phone}`,
+      icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -41,14 +48,14 @@ export default function ContactCard() {
             d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
           />
         </svg>
-
-        <h3 className="text-accent font-semibold tracking-wide">Phone</h3>
-        <p>Call us today for a free consultation and quote!</p>
-        <NextLink href={`tel:${business.phone}`} className="underline">
-          +1 {business.phone}
-        </NextLink>
-      </div>
-      <div className="flex flex-col items-center gap-4 p-6 text-center">
+      ),
+    },
+    {
+      title: "Appointments",
+      description: "Schedule your appointment on our Contact page.",
+      linkText: "Contact Us",
+      linkHref: "/contact",
+      icon: (
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -63,15 +70,29 @@ export default function ContactCard() {
             d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
           />
         </svg>
+      ),
+    },
+  ],
+};
 
-        <h3 className="text-accent font-semibold tracking-wide">
-          Appointments
-        </h3>
-        <p>For inquiries, reach out to us anytime at your convenience.</p>
-        <NextLink href={`mailto:${business.email}`} className="underline">
-          {business.email}
-        </NextLink>
-      </div>
+export default function ContactCard() {
+  return (
+    <div className="grid grid-cols-1 gap-6 laptop:grid-cols-3">
+      {content.sections.map((section, idx) => (
+        <div
+          key={idx}
+          className="flex flex-col items-center gap-4 p-6 text-center"
+        >
+          {section.icon}
+          <h3 className="text-accent font-semibold tracking-wide">
+            {section.title}
+          </h3>
+          <p>{section.description}</p>
+          <NextLink href={section.linkHref} className="underline">
+            {section.linkText}
+          </NextLink>
+        </div>
+      ))}
     </div>
   );
 }
